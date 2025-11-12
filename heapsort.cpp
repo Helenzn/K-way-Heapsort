0a59233946a0d8830b5efaab877eb87609ac0ba2
jgsertoli <jgsertoli@gmail.com>
2025-11-11 22:48:44 -0300

    Implement heapsort structure and functionality and try to use it

--- heapsort.cpp
+++ heapsort.cpp
@@ -36,6 +36,16 @@ int main() {
         n++;
     }
 
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // Estrutura base do heapsort
+    // Checar se ta funcionando corretamente
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+
     cout << "\nVetor:" << endl;
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
